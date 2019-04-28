bd5afd4ae3053f394f58ba932ed23c2f5939ce7c
Dexter Lewis <dexter.lewis@gmail.com>
2019-04-28 12:56:37 -0400

    Removed unused file

--- client/src/Actions/LoginActions.js
+++ client/src/Actions/LoginActions.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 export const IS_LOGGING_IN = 'IS_LOGGING_IN';
 
-export const login = user => (dispatch) => {
+export const login = user => dispatch => {
   dispatch({ type: IS_LOGGING_IN });
 };
