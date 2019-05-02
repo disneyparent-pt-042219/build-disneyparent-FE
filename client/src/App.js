c1eb4a475b49f27407bf540cda3f6f2e15b7084f
Dexter Lewis <dexter.lewis@gmail.com>
2019-05-02 17:37:47 -0400

    enabled private routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 
 import GlobalStyle from './Global';
+import PrivateRoute from './components/PrivateRoute';
 import Login from './components/Pages/Login';
 import Home from './components/Pages/Home';
 import Header from './components/Header/Header';
@@ -12,8 +13,8 @@ function App() {
     <div className="App">
       <Header />
       <Route path="/login" component={Login} />
-      <Route path="/home" component={Home} />
-      <Route path="/post/:id" component={SinglePost} />
+      <PrivateRoute exact path="/home" component={Home} />
+      <PrivateRoute exact path="/post/:id" component={SinglePost} />
       <GlobalStyle />
     </div>
   );
--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -1,54 +1,79 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import { FormButton } from '../Elements/FormButton';
 import { logout } from '../../Actions/UserActions';
 
-function Header(props) {
-  const { user, logout, isLoggedIn } = props;
-  const loggingOut = () => {
+class Header extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: '',
+    };
+  }
+
+  componentDidMount() {
+    this.setState({
+      user: localStorage.getItem('username'),
+    });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.user !== '') {
+      this.setState({ user: localStorage.getItem('username') });
+    }
+  }
+
+  loggingOut = () => {
+    const { logout } = this.props;
     logout();
+    this.setState({
+      user: '',
+    });
+    return <Redirect to="/login" />;
   };
-  return (
-    <header>
-      <nav>
-        <Link to={isLoggedIn ? '/home' : '/login'}>
-          <img src="/images/logo.png" alt="logo" />
-        </Link>
-        <Link to="/login">Parks & Tickets</Link>
-        <Link to="/login">Places to Stay</Link>
-        <Link to="/login">About Us</Link>
-        <Link to="/login">Contact</Link>
-      </nav>
-      {/* if user is logged in, display logout menu */}
-      {isLoggedIn && (
-        <div className="right-nav">
-          <Link to="/attractions">Attractions</Link>
-          <Link to="/posts">Posts</Link>
-          <p>
-            {`Welcome ${user}`}
-            <i className="fas fa-user-circle" />
-          </p>
-          <Link to="/login">
-            <FormButton onClick={loggingOut}>Log Out</FormButton>
+
+  render() {
+    const { user } = this.state;
+    return (
+      <header>
+        <nav>
+          <Link to={user ? '/home' : '/login'}>
+            <img src="/images/logo.png" alt="logo" />
           </Link>
-        </div>
-      )}
-    </header>
-  );
+          <Link to="/login">Parks & Tickets</Link>
+          <Link to="/login">Places to Stay</Link>
+          <Link to="/login">About Us</Link>
+          <Link to="/login">Contact</Link>
+        </nav>
+        {/* if user is logged in, display logout menu */}
+        {user && (
+          <div className="right-nav">
+            <Link to="/attractions">Attractions</Link>
+            <Link to="/posts">Posts</Link>
+            <p>
+              {`Welcome ${user}`}
+              <i className="fas fa-user-circle" />
+            </p>
+            <Link to="/login">
+              <FormButton onClick={this.loggingOut}>Log Out</FormButton>
+            </Link>
+          </div>
+        )}
+      </header>
+    );
+  }
 }
 
 Header.propTypes = {
   user: PropTypes.string.isRequired,
   logout: PropTypes.func.isRequired,
-  isLoggedIn: PropTypes.bool.isRequired,
 };
 
 const mapStateToProps = state => ({
   user: state.user.userName,
-  isLoggedIn: state.user.isLoggedIn,
 });
 
 export default connect(
