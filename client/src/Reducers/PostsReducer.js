969fb9565b56ed28f80ad62e604cd54b0bad78fe
aaharbaugh <aaronmharbaugh@gmail.com>
2019-05-01 21:31:09 -0500

    completed majority of Post Action and reducers

--- client/src/Reducers/PostsReducer.js
+++ client/src/Reducers/PostsReducer.js
@@ -1,11 +1,16 @@
 import {
     FETCHING_POSTS,
     POSTS,
-    POST_ERROR,
+    FETCH_POSTS_ERROR,
     CREATE_NEW_POST,
-    EDIT_POST,
+    POST_CREATED,
+    CREATE_POST_ERROR,
+    EDITING_POST,
+    POST_EDITED,
+    EDIT_POST_ERROR,
     DELETING_POST,
-    DELETED_POST,
+    POST_DELETED,
+    DELETE_POST_ERROR,
   } from '../Actions/PostActions';
 
   const initialState = {
@@ -13,10 +18,86 @@ import {
     postError: '',
     isFetchingPost: false,
     isCreatingPost: false,
+    isEditingPost: false,
+    isDeletingPost: false,
   };
 
   const PostsReducer = (state = initialState, action) => {
     switch (action.type){
+      case FETCHING_POSTS:
+        return {
+          ...state, 
+          isFetchingPost: true,
+        };
+      case POSTS:
+        return {
+          ...state,
+          isFetchingPost: false,
+          posts: action.payload,
+        }
+      case FETCH_POSTS_ERROR:
+        return {
+          ...state,
+          isFetchingPost: false,
+          postError: action.payload,
+        }
+      case CREATING_POST:
+        return {
+          ...state,
+          isCreatingPost: true,
+        }
+      case POST_CREATED:
+        return {
+          ...state,
+          isCreatingPost: false,
+          posts: [...this.state.posts, action.payload],
+        }
+      case CREATE_POST_ERROR:
+        return {
+          ...state,
+          isCreatingPost: false,
+          postError: action.payload,
+        }
+      case EDITING_POST:
+        return {
+          ...state,
+          isEditingPost: true,
+        }
+      case POST_EDITED:
+        return {
+          ...state,
+          isEditingPost: false,
+          posts: this.state.posts.map(post => {
+            post.id === action.payload.id
+              ? { ...action.payload }
+              : post
+          })
+        }
+      case EDIT_POST_ERROR:
+        return {
+          ...state,
+          isEditingPost: false,
+          postError: action.payload,
+        }
+      case DELETING_POST:
+        return {
+          ...state,
+          isDeletingPost: true,
+        }
+      case POST_DELETING:
+        return {
+          ...state,
+          isDeletingPost: false,
+          posts: this.state.posts.filter(post => {
+            post.id !== action.payload.id
+          })
+        }
+      case DELETE_POST_ERROR:
+        return {
+          ...state,
+          isDeletingPost: false,
+          postError: action.payload,
+        }
       default:
         return state;
     }
